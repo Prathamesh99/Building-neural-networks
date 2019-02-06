a4b3e1089c957746bb99822b0e4c8232a356c146
Prathamesh99 <prathamesh.borhade29@gmail.com>
2019-02-06 13:05:41 +0530

    Parameter tuning

--- ann.py
+++ ann.py
@@ -65,12 +65,28 @@ def build_classifier():
 classifier = KerasClassifier(build_fn = build_classifier, batch_size=10, nb_epoch=100)
 accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv=10, n_jobs=-1)
 
+mean = accuracies.mean()
+variation = accuracies.std()
 
+#Parameter tuning
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
 
+def build_classifier(optimizer):
+    classifier = Sequential()
+    classifier.add(Dense(output_dim=6, init='uniform', activation='relu', input_dim=11))
+    classifier.add(Dense(output_dim=6, init = 'uniform', activation='relu'))
+    classifier.add(Dense(output_dim=1, init='uniform', activation='sigmoid'))
+    classifier.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+    return classifier
 
-
-
-
-
-
-
+classifier = KerasClassifier(build_fn = build_classifier)
+parameters = {'batch_size':[25,32],
+              'nb_epoch':[100,500],
+              'optimizer':['adam', 'rmsprop']}
+grid_search_cv = GridSearchCV(estimator = classifier, param_grid = parameters, scoring='accuracy', cv=10)
+grid_search_cv = grid_search_cv.fit(X_train, y_train)
+best_parameters = grid_search_cv.best_estimator_
+best_accuracy = grid_search_cv.best_score_
